c819a4bce63ecd4fda68671b1afe2ed458a0fddb
optimizasean <optimizasean@gmail.com>
2025-12-27 10:48:47 -1000

    Feat: solve 050 with notes on quick refactor

--- src/016.5-ide-superpowers/050-refactor.problem.ts
+++ src/016.5-ide-superpowers/050-refactor.problem.ts
@@ -3,3 +3,6 @@ const func = () => {
   const randomPercentage = `${(Math.random() * 100).toFixed(2)}%`;
   console.log(randomPercentage);
 };
+
+//NOTE can target different things such as randomPercentage with inline variable to reduce code or 
+// extracting the random to a constant in module scope or extracting to a function in module scope instead
