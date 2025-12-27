51e0fc376c24db6db3a2c42f76c5ad3573035135
optimizasean <optimizasean@gmail.com>
2025-12-27 13:29:17 -1000

    Feat: solve 062 - throw error to solve type check

--- src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
+++ src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
@@ -2,6 +2,8 @@ import { Equal, Expect } from "@total-typescript/helpers";
 
 const appElement = document.getElementById("app");
 
+if (!appElement) throw new Error("HTMLElement: appElement is null");
+
 // How do I ensure that appElement is defined?
 
 type Test = Expect<Equal<typeof appElement, HTMLElement>>;
