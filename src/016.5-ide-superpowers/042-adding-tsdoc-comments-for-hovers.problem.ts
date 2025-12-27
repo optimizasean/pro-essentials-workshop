f282749c93ee299114223eae3e0f8873cf10bfda
optimizasean <optimizasean@gmail.com>
2025-12-27 10:15:44 -1000

    Feat: update 042 to tsdoc instead of plain jsdoc

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,19 @@
+/**
+ * Adds two numbers
+ * 
+ * @remarks
+ * Onlu works with numbers, this is part of learning typescript
+ * 
+ * @param a - first number
+ * @param b - second number
+ * @returns The sum of first and second numbers
+ * 
+ * @example
+ * 
+ * myFunction(1, 2)
+ * 
+ * @beta
+ */
 const myFunction = (a: number, b: number) => {
   return a + b;
 };
