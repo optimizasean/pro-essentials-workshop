a50b6e4c15630a71bff3a0fd5a76c822314cefab
optimizasean <optimizasean@gmail.com>
2025-12-27 10:43:18 -1000

    Feat: solve 048 with notes on auto-import

--- src/016.5-ide-superpowers/048-auto-import.problem.ts
+++ src/016.5-ide-superpowers/048-auto-import.problem.ts
@@ -1,6 +1,11 @@
+import { SESSION_EXPIRATION_TIME, SESSION_EXPIRATION_TIME_IN_SECONDS, SESSION_EXPIRATION_TIME_IN_MINUTES, SESSION_EXPIRATION_TIME_IN_HOURS } from "./dummy-import";
+
 const expirationTimes = [
   SESSION_EXPIRATION_TIME,
   SESSION_EXPIRATION_TIME_IN_SECONDS,
   SESSION_EXPIRATION_TIME_IN_MINUTES,
   SESSION_EXPIRATION_TIME_IN_HOURS,
 ];
+
+//NOTE can use autocomplete CTRL SPACE but best if on end of line or might miss something, can also CTRL . for multiple imports or individual
+//The autocomplete could also pull from wrong file so check to make sure the imports are the intended ones!
