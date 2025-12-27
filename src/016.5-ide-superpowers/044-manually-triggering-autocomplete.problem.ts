6e93a19399610b55212c8283e9ca45bd5fa1df1e
optimizasean <optimizasean@gmail.com>
2025-12-27 10:20:24 -1000

    Feat: solve 044

--- src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
+++ src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
@@ -8,11 +8,14 @@ const acceptsObj = (obj: MyObj) => {};
 
 acceptsObj({
   // Autocomplete in here!
+  foo: "wow",
+  bar: 3,
+  baz: false
 });
 
 document.addEventListener(
   // Autocomplete this string!
-  "",
+  "click",
   (event) => {
     console.log(event);
   },
