32ad6e6dfb8d3f640ddaefbcc2d9ed11f6495105
optimizasean <optimizasean@gmail.com>
2025-12-27 10:52:34 -1000

    Feat: solve 051-052

--- src/016.5-ide-superpowers/051-prettier-format-on-save.explainer.ts
+++ src/016.5-ide-superpowers/051-prettier-format-on-save.explainer.ts
@@ -3,3 +3,5 @@
 // Explain how to set up prettier
 
 // Explain how to set up formatOnSave
+
+//Can run in either VS Code extension (through other things in vim) or through git hooks or in project as script
--- src/016.5-ide-superpowers/052-resetting-the-typescript-server.explainer.ts
+++ src/016.5-ide-superpowers/052-resetting-the-typescript-server.explainer.ts
@@ -3,3 +3,5 @@
 // Sometimes TS can get out of sync because it doesn't notice a file changed
 
 // It's always good to have in your back pocket
+
+//CTRL SHIFT P in VSCODE then restart typescript server. It is different in other tools / omnicomplete / linters / ...
