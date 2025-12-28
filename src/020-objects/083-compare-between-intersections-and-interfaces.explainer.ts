551ad317890e27d63fbeff86afe16c7a85fd80ff
optimizasean <optimizasean@gmail.com>
2025-12-27 21:50:55 -1000

    Feat: solve 083 with notes regarding interfaces and types

--- src/020-objects/083-compare-between-intersections-and-interfaces.explainer.ts
+++ src/020-objects/083-compare-between-intersections-and-interfaces.explainer.ts
@@ -1,1 +1,5 @@
 // https://github.com/microsoft/TypeScript/wiki/Performance#preferring-interfaces-over-intersections
+
+//In general probably always use interfaces unless you must (or want to for some reason) use a type
+//TypeScript prefers extending interfaces over types intersections
+//Keeping it basic you might as well use either such as REACT props - at that level both are good enough and equivalent
