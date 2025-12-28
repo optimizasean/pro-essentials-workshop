79666da876c8d1b752f15a94de34cfc4ec63e9bc
optimizasean <optimizasean@gmail.com>
2025-12-27 21:29:07 -1000

    Feat: solve 080.5

--- src/018-unions-and-narrowing/080.5-should-you-provide-function-return-types.explainer.ts
+++ src/018-unions-and-narrowing/080.5-should-you-provide-function-return-types.explainer.ts
@@ -3,22 +3,24 @@
 
 import { Equal, Expect } from "@total-typescript/helpers";
 
-const returnsStringOrNumber = (): string | number => {
+const returnsNumber = (): number => {
   return 1;
 };
 
-const value = returnsStringOrNumber();
+const value = returnsNumber();
 
-if (typeof value === "string") {
-  type test = Expect<Equal<typeof value, never>>;
+// if (typeof value === "string") {
+//   type test = Expect<Equal<typeof value, never>>;
 
-  // @ts-expect-error
-  value.toUpperCase();
-}
+//   // @ts-expect-error
+//   value.toUpperCase();
+// }
+
+//Above required more defensive code, by removing unused return type it gets easier
 
 // 2. PRO: Function return types can help enforce the type of the function
 
-type UserRole = "admin" | "editor" | "viewer";
+type UserRole = "admin" | "editor" | "viewer" | "anonymous";
 
 function getPermissions(role: UserRole): string[] {
   switch (role) {
@@ -28,5 +30,11 @@ function getPermissions(role: UserRole): string[] {
       return ["create", "read", "update"];
     // case "viewer":
     //   return ["read"];
+    default:
+      return [];
   }
 }
+
+//when you add roles not handled in the switch you will need a default or every added role will cause a failure
+
+//Best to use only the return types necessary and not include ones which are not possible or not used to reduce defensive code to catch all the constraints which may not be possible to achieve from any call of the function
