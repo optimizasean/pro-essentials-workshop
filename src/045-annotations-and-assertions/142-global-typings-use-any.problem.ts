c5de0cbdaacb79836bce134b1fcddb12f5bbb9a6
optimizasean <optimizasean@gmail.com>
2026-03-08 21:49:02 -1000

    Feat: solve 142

--- src/045-annotations-and-assertions/142-global-typings-use-any.problem.ts
+++ src/045-annotations-and-assertions/142-global-typings-use-any.problem.ts
@@ -1,7 +1,10 @@
 import { expect, it } from "vitest";
 
 const getObj = () => {
-  const obj = JSON.parse('{ "a": 123, "b": 456 }');
+  const obj: {
+    a: number;
+    b: number;
+  } = JSON.parse('{ "a": 123, "b": 456 }');
 
   return obj;
 };
